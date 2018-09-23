0a69feac2970ce19ae7ec9ac7483350836c0154d
Javier Guerra <javier.jguerra@gmail.com>
2018-09-23 22:01:24 +0100

    ll

--- public/index.js
+++ public/index.js
@@ -5,4 +5,7 @@ socket.on('Hola',function(data){
     console.log(data);
 });
 
+socket.on('client:message', function(data){
+    socket.emit('server:message','Oido cocina, has dicho: ' , data );
+});
 
